36249918fda6be4fe5607052a055941831765e43
eudy1990p <eudy1990@gmail.com>
2016-11-25 22:56:00 -0400

    Organizando proyecto

---
+++ src/java_clinica_plantilla/java_clinica_plantilla.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_clinica_plantilla;
+
+/**
+ *
+ * @author Eudy
+ */
+public class java_clinica_plantilla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
